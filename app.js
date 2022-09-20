e452c869e0a7a2b9d67a520e1d7466dad178d65d
ArgusXronos <jonatanrivera@gmail.com>
2022-09-20 07:05:40 -0500

    Preparing for production

--- app.js
+++ app.js
@@ -1,34 +1,33 @@
-const express = require('express');
-const morgan = require('morgan');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const authJwt = require('./helpers/jwt');
-const errorHandler = require('./helpers/error-handler');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authJwt = require("./helpers/jwt");
+const errorHandler = require("./helpers/error-handler");
 
 const app = express();
 
-require('dotenv/config');
+require("dotenv/config");
 
 // Enable cors to allow conexion from every where
 app.use(cors());
-app.options('*', cors())
+app.options("*", cors());
 
 //Meddleware
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(authJwt());
-app.use('/public/uploads', express.static(__dirname + '/public/uploads'));
+app.use("/public/uploads", express.static(__dirname + "/public/uploads"));
 app.use(errorHandler);
-
 
 // Global parameters
 const api = process.env.API_URL;
 
 // Routers Parameters
-const productRouter = require('./routers/productsRouter');
-const categoriesRouter = require('./routers/categoriesRouter');
-const ordersRouter = require('./routers/ordersRouter');
-const usersRouter = require('./routers/usersRouter');
+const productRouter = require("./routers/productsRouter");
+const categoriesRouter = require("./routers/categoriesRouter");
+const ordersRouter = require("./routers/ordersRouter");
+const usersRouter = require("./routers/usersRouter");
 
 // Routers
 app.use(`${api}/products`, productRouter);
@@ -36,18 +35,24 @@ app.use(`${api}/categories`, categoriesRouter);
 app.use(`${api}/users`, usersRouter);
 app.use(`${api}/orders`, ordersRouter);
 
-mongoose.connect(process.env.CONNECTION_STRING, {
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'shanyo-database'
-})
-.then(() => {
-    console.log('Database Connection is ready...')
-})
-.catch((err) => {
+    dbName: "shanyo-database",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
     console.log(err);
-})
-
-app.listen(3000, () => {
-    console.log('server is running http://localhost:3000');
-})
+  });
+// Development
+// app.listen(3000, () => {
+//   console.log("server is running http://localhost:3000");
+// });
+// Production
+var server = app.listen(process.env.PORT || 3000, function () {
+  var port = server.address().port;
+  console.log("Express is working on port:" + port);
+});
