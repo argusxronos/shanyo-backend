48ed29aac74ac7e5182462900425539392261b12
ArgusXronos <jonatanrivera@gmail.com>
2022-10-01 10:58:45 -0500

    01/10/2022 jrivera Fixing login method.

--- helpers/error-handler.js
+++ helpers/error-handler.js
@@ -1,15 +1,25 @@
-function errorhandler(err, req, res, next) {
-    console.log('Error Handling Middleware called.');
-    // jwt authentication error
-    if (err.name === 'UnauthorizedError') {
-        return res.status(401).json({success: false, message: "The User is no authorized", error: err})
-    }
-    // Validation Error
-    if (err.name === 'ValidationError') {
-        return res.status(401).json({success: false, message: 'Validation Error', error:  err})
-    }
-    // default to 500 server error
-    return res.status(500).json({success: false, message: 'Unauthorized Operation!!!', error: err});
+function errorHandler(err, req, res, next) {
+  console.log("Error Handling Middleware called.");
+  // jwt authentication error
+  if (err.name === "UnauthorizedError") {
+    return res
+      .status(401)
+      .json({
+        success: false,
+        message: "The User is no authorized",
+        error: err,
+      });
+  }
+  // Validation Error
+  if (err.name === "ValidationError") {
+    return res
+      .status(401)
+      .json({ success: false, message: "Validation Error", error: err });
+  }
+  // default to 500 server error
+  return res
+    .status(500)
+    .json({ success: false, message: "Unauthorized Operation!!!", error: err });
 }
 
-module.exports = errorhandler;
+module.exports = errorHandler;
